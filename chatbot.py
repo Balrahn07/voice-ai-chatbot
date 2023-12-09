1c9359a2803eb3bead1238b060e9ebe44742ead1
Balrahn <dnz.akbs@gmail.com>
2023-12-10 00:50:45 +0100

    Add continuous conversation feature

--- chatbot.py
+++ chatbot.py
@@ -1,26 +1,46 @@
+import os
 import time
 import openai
 import elevenlabs
 
 
-start_time = time.time()
+timestamp = time.strftime("%Y_%m_%d-%H_%M_%S", time.gmtime())
+filename = "ai_chatbot_conversation" + timestamp + ".txt"
 
-openai.api_key = "API_KEY"
-content = input("How can I help you?\n")
-
-completion = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": content}],
-)
-chat_response = completion.choices[0].message.content
-
+# openai chatgpt
+openai.api_key = "OPENAI_API_KEY"
 # elevenlabs text to speech
-elevenlabs.set_api_key("API_KEY")
+elevenlabs.set_api_key("ELEVENLABS_API_KEY")
 
-# Convert the response to audio and play it
-audio = elevenlabs.generate(text=chat_response, voice="Bella")
+conversation = []
 
-print("\nAI:", chat_response, end="\r\n")
-print("--- %s seconds ---" % (time.time() - start_time))
+while True:
+    
+    content = input("Message chatbot: \n")
+    start_time = time.time()
+    if (content=="quit"):
+        break
+    
+    conversation.append({"role": "user", "content":content})
 
-elevenlabs.play(audio)
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+    )
+
+    chat_response = completion.choices[0].message.content
+    conversation.append({"role": "assistant", "content": chat_response})
+    
+    # Convert the response to audio and play it
+    audio = elevenlabs.generate(text=chat_response, voice="Bella")
+
+    print("\nChatbot:", chat_response, end="\r\n")
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    with open(filename, 'a') as f:
+        f.write("User: " + content + "\n")
+        f.write("Chatbot: " + chat_response + "\n")
+
+    elevenlabs.play(audio)
+
+print("Chat session is terminated.")
